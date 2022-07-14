c46d9dc30fcd6d0bd64b9b70a7bf6a17647ecd26
Gustavo Joseh <gustavojoseh2@gmail.com>
2022-07-14 13:12:02 -0300

    first commit

---
+++ Inicio/dados2.js
@@ -0,0 +1,10 @@
+let preco = 19.99
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto)
+
+console.log(19.9 * 0.6);
+console.log(precoComDesconto);
+
+let nome = "caderno"
+let categoria = "papelaria"
+console.log("Nome: " + nome + ", Categoria: " + categoria)
