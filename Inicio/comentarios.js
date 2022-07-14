f4b764a1dcc7c9eb290f4f80bf9ba3d83f38ced1
Gustavo Joseh <gustavojoseh2@gmail.com>
2022-07-14 11:50:28 -0300

    fist commit

---
+++ Inicio/comentarios.js
@@ -0,0 +1,8 @@
+// console.log é usado para exibir algo na tela do computador
+/*
+console.log é usado para exibir algo na tela do computador
+comentario de multiplas linhas
+*/
+console.log(2); // console.log é usado para exibir algo na tela do computador
+console.log("sei la");
+console.log('sei la');
---
+++ Inicio/sentença2.js
@@ -0,0 +1,3 @@
+console.log(2);
+console.log("sei la");
+console.log('sei la');
