50b20180c7e9785e96de4a556cf5663e2a6cb351
Gustavo Joseh <gustavojoseh2@gmail.com>
2022-07-18 23:03:12 -0300

    first commit

---
+++ fundamentos/booleans.js
@@ -0,0 +1,17 @@
+let ativo = true
+console.log(ativo)
+
+ativo = false
+console.log(ativo)
+
+ativo = 1
+console.log(!!ativo)
+
+console.log('hakuna matata')
+console.log(!!3)
+console.log(!!-3)
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(ativo = true))
